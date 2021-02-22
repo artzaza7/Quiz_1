925fa7fe191f55893b60cb3c7f580367f2c4f572
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-22 18:24:23 +0700

    ข้อ 2 แก้ไข

--- 2.c
+++ 2.c
@@ -5,12 +5,12 @@ int main()
     int check=0;
     int boom=0;
     scanf("%ld",&number);
-    int min=number-1,max=number+1;
-    while(check==0)
+    long int min=number-1,max=number+1;
+    while(check==0&&number!=1&&number!=0&&number!=2)
     {
         for(int i=2;i<number;i++)
         {
-            if(number%i==0)1
+            if(number%i==0)
             {
                 check=2;
                 break;
@@ -19,7 +19,11 @@ int main()
                 check=1;
         }   
     }
-    if(check==2)
+    if(number==1||number==0||number==2)
+    {
+        printf("%ld",number);
+    }
+    else if(check==2)
     {
         for(int i=number-1;i>10&&boom!=1;i--)
         {
@@ -38,14 +42,13 @@ int main()
             }
         }
         boom=0;
-        for(int i=number;i<=number+2&&boom!=1;i++)
+        for(int i=number;i<=number+10&&boom!=1;i++)
         {
             for(int j=2;j<10;j++)
             {
                 if(i%j==0)
                 {
                     boom=2;
-                    max=number;
                     break;
                 }
                 else
@@ -55,11 +58,11 @@ int main()
                 }
             }
         }
-        printf("%d %d",min,max);
+        printf("%ld %ld",min,max);
     }
     else
     {
-        printf("%d",number);
+        printf("%ld",number);
     }
     return 0;
 }
